3ddffa6d0dd0c8417d5d9927a9098f7dd591c66e
SaVannah Hussey <shussey1@uwyo.edu>
2024-11-11 17:37:38 +0000

    Finished with Code!

--- cosc1010-lab09.py
+++ cosc1010-lab09.py
@@ -3,7 +3,8 @@
 # 11/11/2024
 # Lab 09
 # Lab Section:14
-# Sources, people worked with, help given to:
+# Sources, people worked with, help given to: Textbook Chapters: 8, 9,
+# COSC1010_lec12-Classes.pptx.pdf, COSC1010_lec11-Functions.pptx.pdf,
 # Your
 # Comments
 # Here
